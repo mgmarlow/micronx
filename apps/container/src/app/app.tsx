001762b71c456271cb8db50948bd62d50f8b89a4
Graham Marlow <graham@onesignal.com>
2021-08-02 08:39:29 -0700

    Add bulma

--- apps/container/src/app/app.tsx
+++ apps/container/src/app/app.tsx
@@ -6,9 +6,34 @@ const dashboardHost = 'http://localhost:3001';
 const settingsHost = 'http://localhost:3002';
 
 const Nav = () => (
-  <nav>
-    <Link to="/">Dashboard</Link>
-    <Link to="/settings">Settings</Link>
+  <nav className="navbar" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <p className="navbar-item">Micronx</p>
+
+      <a
+        role="button"
+        className="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" className="navbar-menu">
+      <div className="navbar-start">
+        <Link className="navbar-item" to="/">
+          Dashboard
+        </Link>
+
+        <Link className="navbar-item" to="/settings">
+          Settings
+        </Link>
+      </div>
+    </div>
   </nav>
 );
 
--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -1,5 +1,55 @@
 function App() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="container">
+      <div className="section">
+        <h1 className="title">Dashboard</h1>
+      </div>
+
+      <div className="section">
+        <div className="tile is-ancestor">
+          <div className="tile is-vertical is-8">
+            <div className="tile">
+              <div className="tile is-parent is-vertical">
+                <article className="tile is-child notification is-primary">
+                  <p className="title">Vertical...</p>
+                  <p className="subtitle">Top tile</p>
+                </article>
+                <article className="tile is-child notification is-warning">
+                  <p className="title">...tiles</p>
+                  <p className="subtitle">Bottom tile</p>
+                </article>
+              </div>
+              <div className="tile is-parent">
+                <article className="tile is-child notification is-info">
+                  <p className="title">Middle tile</p>
+                  <p className="subtitle">With an image</p>
+                  <figure className="image is-4by3">
+                    <img src="https://bulma.io/images/placeholders/640x480.png" />
+                  </figure>
+                </article>
+              </div>
+            </div>
+            <div className="tile is-parent">
+              <article className="tile is-child notification is-danger">
+                <p className="title">Wide tile</p>
+                <p className="subtitle">Aligned with the right tile</p>
+                <div className="content">{/* content */}</div>
+              </article>
+            </div>
+          </div>
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-success">
+              <div className="content">
+                <p className="title">Tall tile</p>
+                <p className="subtitle">With even more content</p>
+                <div className="content">{/* content */}</div>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
